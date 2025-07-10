a115b920c4919305d970b4eebb0b3780cea192cb
dilankavishka <dkgamage01@gmail.com>
2025-07-10 15:09:48 +0530

    Initial commit

---
+++ BinaryInCSharpDemo/Program.cs
@@ -0,0 +1,6 @@
+﻿
+int binaryValue = 1001;
+string stringRepresentation = Convert.ToString(binaryValue, 2).PadLeft(32, '0');
+
+Console.WriteLine($"Binary value: {binaryValue} (base 10)");
+Console.WriteLine($"Binary Value: {stringRepresentation} (base 2)");
