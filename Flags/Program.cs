7ca6bb67fdba17960614ab680b8ad7f37ba5a66d
dilankavishka <dkgamage01@gmail.com>
2025-07-12 13:44:24 +0530

    flags

---
+++ Flags/Program.cs
@@ -0,0 +1,19 @@
+﻿
+Console.WriteLine(ConvertToBinary((int)SystemPermissions.Reddis));
+
+string ConvertToBinary(int value, int places = 8)
+{
+    return Convert.ToString(value, 2).PadLeft(places, '0');
+}
+
+[Flags]
+enum SystemPermissions
+{
+    None = 0,
+    SQL = 1, //00000001
+    WebServer = 1 << 1, //00000010
+    Reddis = 1 << 2, //00000100
+    Azure = 1 << 3, //00001000
+    Github = 1 << 4, //00010000
+    BuidSystem = 1 <<5 //00100000
+}
