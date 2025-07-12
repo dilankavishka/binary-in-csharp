41332559ca55809f9a584cdf9f332dc674bb8b8d
dilankavishka <dkgamage01@gmail.com>
2025-07-12 09:39:35 +0530

    OR AND operators

---
+++ BinaryOperators/Program.cs
@@ -0,0 +1,18 @@
+﻿int value1 = 0b1001;
+int value2 = 0b1010;
+
+Console.WriteLine($"$The value 1 is {ConvertToBinary(value1)}");
+Console.WriteLine($"$The value 2 is {ConvertToBinary(value2)}");
+
+// OR Operator
+int result = value1 | value2;
+Console.WriteLine($"$The result is {ConvertToBinary(result)}");
+
+// AND Operator
+int result1 = value1 & value2;
+Console.WriteLine($"$The result is {ConvertToBinary(result1)}");
+
+string ConvertToBinary(int value, int places = 8)
+{
+    return Convert.ToString(value, 2).PadLeft(places, '0');
+}
